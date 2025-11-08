4fa37e9ec5df0e21206f5a4454d50ff820cb22a0
is13mael <ismaelparramoussaif@gmail.com>
2025-11-08 12:25:20 +0100

    .

--- src/App.java
+++ src/App.java
@@ -1,26 +1,32 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.print("Por favor, introduzca la altura (número mayor igual a 5): ");
-        int altura = Integer.parseInt(System.console().readLine());
+        try {
+            System.out.print("Por favor, introduzca la altura (número mayor igual a 5): ");
+            int altura = Integer.parseInt(System.console().readLine());
 
-        int AlturaInt = (altura - 3)/2;
+            int AlturaInt = (altura - 3) / 2;
 
-        if (altura % 2 != 0 && altura >=5) {
-            System.out.println("MMMMMM");
+            if (altura % 2 != 0 && altura >= 5) {
+                System.out.println("MMMMMM");
 
-            for(int i = 0; i<AlturaInt; i++){
-                System.out.println("M    M");
+                for (int i = 0; i < AlturaInt; i++) {
+                    System.out.println("M    M");
+                }
+
+                System.out.println("MMMMMM");
+
+                for (int i = 0; i < AlturaInt; i++) {
+                    System.out.println("M    M");
+                }
+
+                System.out.println("MMMMMM");
+            } else {
+                System.out.println("La altura introducida no es la correcta.");
             }
-
-            System.out.println("MMMMMM");
-
-            for(int i = 0; i<AlturaInt; i++){
-                System.out.println("M    M");
-            }
-
-            System.out.println("MMMMMM");
-        }else{
-            System.out.println("La altura introducida no es la correcta.");
+        } catch (NumberFormatException e) {
+            System.out.println("Número mal introducido.");
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error inesperado.");
         }
     }
 }
